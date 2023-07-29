2ff3eb95932cf8ee4617685e2f84a63fb0f86b92
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-07-29 22:39:33 +0800

    Add to ImageViewer.js

--- js/ui/ImageViewer.js
+++ js/ui/ImageViewer.js
@@ -9,6 +9,9 @@ class ImageViewer {
     this.imageList = this.viewer.querySelector('.images-list');
     this.drawImages = this.drawImages.bind(this);
     this.images = [];
+    this.selectButton = this.viewer.querySelector('button.select-all');
+    this.showButton = this.viewer.querySelector('button.show-uploaded-files');
+    this.sendButton = this.viewer.querySelector('button.send');
 
     this.registerEvents();
   }
@@ -23,13 +26,42 @@ class ImageViewer {
    * 5. Клик по кнопке "Отправить на диск" открывает всплывающее окно для загрузки файлов
    */
   registerEvents(){
+    this.checkButtonText();
     this.viewer.addEventListener('click', (e) => {
+      e.preventDefault();
       const target = e.target;
-      target.classList.toggle('selected');
+      if (target.tagName === 'IMG'){
+        target.classList.toggle('selected');
+        this.checkButtonText();
+        return
+      }
     })
+    
     this.viewer.addEventListener('dblclick', (e) => {
+      e.preventDefault();
       const target = e.target;
-      this.preview.src = target.src;
+      if (target.tagName === 'IMG') {
+        this.preview.src = target.src;
+      }
+    })
+
+    this.selectButton.addEventListener('click', (e) => {
+      if(this.selectButton.textContent === "Выбрать всё"){
+        ;[...this.images].forEach(el => {
+          const img = el.querySelector('img');
+          if( img ){
+            img.classList.add('selected');
+          }
+        })
+      }else{
+        ;[...this.images].forEach(el => {
+          const img = el.querySelector('img.selected');
+          if(img){
+            img.classList.remove('selected');
+          }
+        })
+      }
+      this.checkButtonText();
     })
   }
 
@@ -56,14 +88,25 @@ class ImageViewer {
       this.imageList.appendChild(image);
       this.images.push(image);
     });
-    
   }
 
   /**
    * Контроллирует кнопки выделения всех изображений и отправки изображений на диск
    */
   checkButtonText(){
+    if(this.images.length > 0){
+      this.selectButton.classList.remove('disabled');
+    }else {
+      this.selectButton.classList.add('disabled');
+    }
 
+    if(this.images.some(el => el.querySelector('img').classList.contains('selected'))){
+      this.selectButton.textContent = "Снять выделение";
+      this.sendButton.classList.remove('disabled');
+    }else{
+      this.selectButton.textContent = "Выбрать всё";
+      this.sendButton.classList.add('disabled');
+    }
   }
 
 }
